fc2a69530a5646dcf04ca505ee772c89e7659ed5
Tenzin Gyaltsen <tenzingyaltsen16@gmail.com>
2024-10-17 01:21:44 -0400

    Initial commit.

---
+++ Script.R
@@ -0,0 +1,8 @@
+# Kobayashi Maru
+
+# Import data.
+raw_data <- read.csv("study2_n1.csv")
+working <- raw_data
+
+#' No treatment --> higher QoL? Explanation might be possibly less severe 
+#' disease and also effect of chemotherapy.
