286faf9ed46b107d87cbca388639ad3997ad4334
Tenzin Gyaltsen <tenzingyaltsen16@gmail.com>
2024-10-22 12:02:52 -0400

    Commit with an initial plan and the first intro done.

--- Script.R
+++ Script.R
@@ -1,8 +1,22 @@
 # Kobayashi Maru
 
+#Install and load packages.
+install.packages("dplyr")
+install.packages("ggplot2")
+library(dplyr)
+library(ggplot2)
+
 # Import data.
 raw_data <- read.csv("study2_n1.csv")
 working <- raw_data
 
 #' No treatment --> higher QoL? Explanation might be possibly less severe 
 #' disease and also effect of chemotherapy.
+str(working)
+glimpse(working)
+summary(working)
+status(working)
+
+# Remove irrelevant variables and recode factors.
+
+# EDA.
